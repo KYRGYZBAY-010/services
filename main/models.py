3a4e107543d191f8d6ca3233c0fb422c8cb89acf
kyrgyzbay-010 <emirlanmaratov8@gmail.com>
2022-09-15 16:10:35 +0600

    корень

---
+++ main/models.py
@@ -0,0 +1,17 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+class Construction_and_repair(models.Model): #Строительство и ремонт
+    pass
+
+
+class Auto_services(models.model):
+    pass
+
+
+class Deauty_and_health(models.model):
+    pass
+
+
+class Equipment_repair(models.model):
+    pass
